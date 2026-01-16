eb1c917f1c84397afde5edbda895ea36b719a7b0
tutorialmonke123-eng <tutorialmonke123@gmail.com>
2026-01-16 14:59:55 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -25,7 +25,7 @@ export default {
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
-                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-if="i + 1 <= 75" class="type-label-lg">#{{ i + 1 }}</p>
                             <p v-else class="type-label-lg">Legacy</p>
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
@@ -53,6 +53,10 @@ export default {
                         <li>
                             <div class="type-title-sm">Password</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">Hand cam</div>
+                            <p>{{ level.handcam || 'Not Needed' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>
@@ -100,7 +104,7 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 240fps)
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
